8f3a04948fb91696c3b282815e5c24b86391d591
Aniket Karna <57065053+CrypticNumbers8@users.noreply.github.com>
2023-07-22 20:18:36 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js (or index.js)
+
+import React from "react";
+import Dashboard from "./components/Dashboard";
+
+const App = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
